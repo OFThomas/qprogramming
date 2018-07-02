a224efdbf4b4736e2f300a779c0bd7b63a8ca847
Oliver <ot17409@bristol.ac.uk>
2018-07-02 15:52:57 +0100

    added buttons

--- longtermcode/mess.py
+++ longtermcode/mess.py
@@ -1,26 +1,49 @@
 from tkinter import *
 from tkinter import messagebox
+from functools import partial
 
-class Application(Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.pack()
-        self.create_widgets()
+class OpenToplevels(Frame):
+    #open different windows
+    def __init__(self):
+        self.root = Tk()
+        self.button_ctr=0
+        """
+        but=Button(self.root, text="Open a Toplevel",
+                      command=self.open_another)
+        but.grid(row=0, column=0)
+        """
+        menubut=Button(self.root, text='Work pls', command=self.menus)
+        menubut.grid(row=0, column=0)
+                
+        Button(self.root, text="Exit Tkinter", bg="red",
+                  command=self.root.quit).grid(row=1, column=0, sticky="we")
+        self.root.mainloop()
+    
+    def menus(self):
+        self.button_ctr += 1
+        id = Toplevel(self.root)
+        id.title("Toplevel #%d" % (self.button_ctr))
+        Button(id, text="Close Toplevel #%d" % (self.button_ctr),
+                  command=partial(self.close_it, id),
+                  bg="orange", width=20).grid(row=100, column=0)
+        
+        hi_there = Button(id, fg="white", bg="blue")
+        hi_there["text"] = "Hello World\n Do you want to do some quantum programming? \n(click me)"
+        # do stuff
+        hi_there["command"] = self.begin
+        #put at the top of the screen
+        hi_there.grid(row=0, column=0)
 
-    def create_widgets(self):
-        self.hi_there = Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
 
-        self.quit = Button(self, text="QUIT", fg="red",
-                              command=root.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
+    def begin(self):
         print("hi there, everyone!")
+       
+    def close_it(self, id):
+        id.destroy()
 
-root = Tk()
-app = Application(master=root)
-app.mainloop()
+ot=OpenToplevels()
+
+
+
+
 
