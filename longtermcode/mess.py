8eed6f67a8b5fbf0f932e6a74aee38545c8f903e
johnrscott <johnrscott0@gmail.com>
2018-07-03 14:17:19 +0100

    Updated

--- longtermcode/mess.py
+++ longtermcode/mess.py
@@ -5,90 +5,114 @@ from shor_alg import shor
 from tkinter.messagebox import showinfo
 import webbrowser
 
+
+class MakeButton():
+
+    def __init__(self,frame,row,col,text,fun):
+        self.fun = fun
+        self.button = Button(frame, fg="white",
+                             bg="blue",text=text,
+                             width=20,relief=RAISED,
+                             command=self.fun)
+        self.button.grid(row=row,column=col)
+
+    def set_fun(self,new_fun):
+        self.fun = new_fun
+
+    def set_text(self,new_text):
+        self.button.config(text=new_text)
+
+    def set_position(self,row,col,sticky):
+        self.button.grid(row=row,column=col,sticky=sticky)
+
+    def set_color(self,foreground,background):
+        self.button.config(fg=foreground,bg=background)
+        
+    
+
 class OpenToplevels(Frame):
     #open different windows
     
     def __init__(self):
-        # make window called self.root 
+        # make window called self.root
         self.root = Tk()
         self.root.resizable(width=TRUE,height=TRUE)
         self.root.geometry('{}x{}'.format(640,480))
-
-        self.button_ctr=0
+        
         #get that professional font
-        font=('Comic Sans MS', 12)
+        font=('Comic Sans MS', 15)
         self.root.option_add("*Font", font)
         
         # create all of the main containers
-       # top_frame = Frame(self.root, bg='cyan', width=450, height=50, pady=3)
-        center = Frame(self.root, bg='white', width=50, height=40, padx=3, pady=3)
+        # top_frame = Frame(self.root, bg='cyan', width=450, height=50, pady=3)
+        self.center = Frame(self.root, bg='white', width=200, height=40, padx=3, pady=3)
         #btm_frame = Frame(self.root, bg='white', width=450, height=45, pady=3)
 
         # layout all of the main containers
         self.root.grid_rowconfigure(0, weight=1)
         self.root.grid_columnconfigure(0, weight=1)
 
-        center.grid(row=1, sticky="nsew")
+        self.center.grid(row=1, sticky="nsew")
 
         # create the center widgets
-        center.grid_rowconfigure(0, weight=1)
-        center.grid_columnconfigure(0, weight=1)
+        self.center.grid_rowconfigure(0, weight=1)
+        self.center.grid_columnconfigure(0, weight=1)
 
-        ctr_ltop = Frame(center, bg='blue', width=10, height=10)
-        ctr_ltop.grid(row=0, column=0, sticky="nsew")
+        self.ctr_ltop = Frame(self.center, bg='blue', width=10, height=10, padx=3, pady=3)
+        self.ctr_ltop.grid(row=0, column=0, sticky="nsw")
         
-        ctr_lmid = Frame(center, bg='yellow', width=10, height=10, padx=3, pady=3)
-        ctr_lmid.grid(row=1, column=0, sticky="nsew")
+        self.ctr_lmid = Frame(self.center, bg='yellow', width=10, height=10, padx=3, pady=3)
+        self.ctr_lmid.grid(row=1, column=0, sticky="nsw")
         
-        ctr_lbot = Frame(center, bg='green', width=10, height=10, padx=3, pady=3)
-        ctr_lbot.grid(row=2,column=0, sticky="nsew")
+        self.ctr_lbot = Frame(self.center, bg='green', width=10, height=10, padx=3, pady=3)
+        self.ctr_lbot.grid(row=2,column=0, sticky="nsw")
 
-        ctr_rtop = Frame(center, bg='blue', width=10, height=10)
-        ctr_rtop.grid(row=0, column=1, sticky="nsew")
+        self.ctr_rtop = Frame(self.center, bg='orange', width=10, height=10, padx=3, pady=3)
+        self.ctr_rtop.grid(row=0, column=1, sticky="nsew")
         
-        ctr_rmid = Frame(center, bg='yellow', width=10, height=10, padx=3, pady=3)
-        ctr_rmid.grid(row=1, column=1, sticky="nsew")
+        self.ctr_rmid = Frame(self.center, bg='cyan', width=10, height=10, padx=3, pady=3)
+        self.ctr_rmid.grid(row=1, column=1, sticky="nsew")
         
-        ctr_rbot = Frame(center, bg='green', width=10, height=10, padx=3, pady=3)
-        ctr_rbot.grid(row=2,column=1, sticky="nsew")
-
-
+        self.ctr_rbot = Frame(self.center, bg='purple', width=10, height=10, padx=3, pady=3)
+        self.ctr_rbot.grid(row=2,column=1, sticky="nsew")
 
         txtwdth=20
-
-        hi_text="Do you want to do some quantum programming? \n(click me)"
+        
         #top
-        self.B0=Button(self.root, fg="white", bg="blue",text=hi_text,width=2*txtwdth,
-                  relief=RAISED,command=self.begin)
-        self.B0.grid(row=0, column=0, columnspan=2)
+
+        self.B1 = MakeButton(self.ctr_ltop,0,0,"Project Q this way!",run_projectq)
+        self.B2 = MakeButton(self.ctr_rtop,0,1,"Pyquil!",run_pyquil)
+        self.B3 = MakeButton(self.ctr_lmid,1,0,"Qiskit!",run_qiskit)
+        self.B4 = MakeButton(self.ctr_rmid,1,1,"Q#!",run_pyquil)
+        self.B5 = MakeButton(self.ctr_lbot,2,0,"Shor!",run_shor)
+
+        self.B6 = MakeButton(self.root,100,0,"Exit Tkinter",self.root.quit)
+
+        #self.B6 = MakeButton(self.root,100,0,"Exit Tkinter", bg="red",
+        #                     command=self.root.quit).grid(row=100, column=0, columnspan=2, sticky="we")
         
-        self.B1=Button(center, text="Project Q this way!",width=txtwdth,
-                  command=run_projectq)
-        self.B1.grid(row=0,column=0)
-        #mid
-        self.B2=Button(center, text="Pyquil",width=txtwdth,
-                  command=run_pyquil)
-        self.B2.grid(row=0,column=1)
         
-        self.B3=Button(center, text="Qiskit",width=txtwdth,
-                  command=run_qiskit)
-        self.B3.grid(row=1,column=0)
-        #bot
-        self.B4=Button(center, text="Q#",width=txtwdth,
-                      command=run_qsharp)
-        self.B4.grid(row=1,column=1)
-        
-        self.B5=Button(center, text="shor",width=txtwdth,
-                       command=lambda:self.change_text(self.B4,"New text"))
-        self.B5.grid(row=2,column=0)
-        
-        shorenterval=Button(center, text='Shors value', width=txtwdth,
+        shorenterval=Button(self.ctr_rbot, text='Shors value', width=txtwdth,
                 command=self.usr_input)
-        shorenterval.grid(row=2,column=1)  
+        shorenterval.grid(row=2,column=1)
+
+
+        self.B0=Button(self.root, fg="white", bg="blue",text="Quantum Programming Guide",width=2*txtwdth,
+                  relief=RAISED,command=self.begin)
+        self.B0.grid(row=90, column=0, columnspan=2, sticky="we")
+        
         #exit
         self.B6=Button(self.root, text="Exit Tkinter", bg="red",
                   command=self.root.quit).grid(row=100, column=0, columnspan=2, sticky="we")
+
+        self.button_0 = MakeButton(self.root,0,0,"Hello",lambda:print(10))
+        
         self.root.mainloop()
+        
+
+    def makebackground(self):
+        print()
+    
         
     def begin(self):
         print("hi there, everyone!")
