46b203bf1b47b8087f57789591cee312941c6c43
Oliver <ot17409@bristol.ac.uk>
2018-07-05 17:10:37 +0100

    background working 4 frames

--- longtermcode/background.py
+++ longtermcode/background.py
@@ -4,33 +4,17 @@ from tkinter import messagebox
 from tkinter.messagebox import showinfo
 import webbrowser
 from makestuff import *
+from control import *
 
 class Background(Frame):
     #open different windows
     
     def __init__(self):
         self.setgeometry() 
-        #make frames for backgrounds and splitting the widgits
-        self.view = MakeFrame(self.root,'black',100,100,0,0,'nsew')
-        self.input = MakeFrame(self.view.frame,'green',100,100,0,0,'nsew')
-        self.output = MakeFrame(self.view.frame,'pink',100,100,0,1,'nswe')
+        self.buildframe() 
         #here be buttons 
-        self.control = MakeFrame(self.root, 'white',200, 40,1,0,'nsew')  
-        self.interact = MakeIO(self.input,self.output)
         # documentation
-        self.docframe=MakeFrame(self.root, 'lime', 100,100,20,0,'nesw')
-       
-        doclabels=[['Quantum programming guide',lambda:None], 
-                ['Riggeti docs',lambda:None],
-                ['Qiskit docs',lambda:None],
-                ['Project Q docs',lambda:None]]
-
-        #for i in range(0,len(doclabels)):
-        #    self.Bdocs[i]=MakeButton(self.docframe.frame, i,0,)
-
-        self.colors=['blue', 'yellow', 'green', 'orange', 'cyan', 'purple']
-        
-
+        self.control=Control(self.controlframe.frame)
         self.root.mainloop()        
 
     def setgeometry(self):
@@ -50,6 +34,19 @@ class Background(Frame):
         self.root.option_add("*Font", font)
         
     def buildframe(self):
+        #make frames for backgrounds and splitting the widgits
+        self.docframe=MakeFrame(self.root, 'lime', 100,100,0,0,'nesw')
+        self.viewframe = MakeFrame(self.root,'black',100,100,1,0,'nsew')
+        self.controlframe = MakeFrame(self.root, 'white',200, 100,2,0,'nsew')  
+        self.exitfram=MakeFrame(self.root, 'cyan', 100,100,3,0,'nsew')
         return 
+
+    def makedocs(self):
+         doclabels=[['Quantum programming guide',lambda:None], 
+                ['Riggeti docs',lambda:None],
+                ['Qiskit docs',lambda:None],
+                ['Project Q docs',lambda:None]]
+        #self.b0=MakeButton()
+
 
 backgroundframe=Background()
