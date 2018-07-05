4a45dc3d59c9da2e66c8ca07330590ed26d08f34
johnrscott <johnrscott0@gmail.com>
2018-07-05 12:06:24 +0100

    Changed shor

--- longtermcode/quantumprograms.py
+++ longtermcode/quantumprograms.py
@@ -3,7 +3,8 @@ from tkinter.messagebox import showinfo
 from shor_projectq import shor
 
 def run_shor(n):
-    return shor(n)[0]
+    x = shor(n)
+    return x[0]
         
 def run_projectq():
     s="project q"
@@ -13,7 +14,7 @@ def run_projectq():
 def run_qsharp():
     print('no q# for you')
     s="q#"
-    showinfo(title="reply", message="not implemented %s yet!" % (s) )
+./    showinfo(title="reply", message="not implemented %s yet!" % (s) )
     print(s)
 
 def run_qiskit():
