29def45889dfb408172d1361c14df9e5df74bf0c
johnrscott <johnrscott0@gmail.com>
2018-07-06 13:05:51 +0100

    Added dynamic button positions

--- longtermcode/control.py
+++ longtermcode/control.py
@@ -6,14 +6,17 @@ class Control():
     def __init__(self,frame):
         self.frame = frame
         self.buttons = {}
-        self.row = 0
-        self.make_button("test",partial(print,15))
+        self.row, self.column = 0,0
         
     def make_button(self,label,cmd):
         print('cmd', cmd)
         self.buttons[label] = MakeButton(self.frame,2,0,"Submit",cmd)
-        self.buttons[label].button.grid(row=self.row,column=0)
-        self.row = self.row + 1
+        self.buttons[label].button.grid(row=self.row,column=self.column)
+        self.column = self.column + 1 
+        if self.column == 2:
+            self.column = 0
+            self.row = self.row + 1
+            if self.row == 3: exit(1)
 
     def clear_all(self):
         for key in self.input:
