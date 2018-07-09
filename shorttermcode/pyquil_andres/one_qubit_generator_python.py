de190591816444d65843a127018b08bc5545c75b
Oliver <ot17409@bristol.ac.uk>
2018-07-09 14:33:19 +0100

    renamed to .py

--- shorttermcode/pyquil_andres/one_qubit_generator_python.py
+++ shorttermcode/pyquil_andres/one_qubit_generator_python.py
@@ -1,19 +1,24 @@
 import numpy as np
+
 # State initialisation
 state0=np.array([[1],[0]])
+
 # State manipulation
 H=(1/np.sqrt(2))*np.array([[1,1],[1,-1]])
 state1=np.dot(H,state0)
 phi=np.pi/2
 PHASE=np.array([[1,0],[0,np.exp(1j*phi)]])
 state2=np.dot(PHASE,state1)
+
 # Measurement: defining projectors P0,P1
 ket0=np.array([[1],[0]])
 ket1=np.array([[0],[1]])
 P0=np.dot(ket0,ket0.T)
 P1=np.dot(ket1,ket1.T)
+
 # Probability of obtaining outcome 0
 prob0=np.trace(np.dot(P0,np.dot(state2,np.conj(state2).T)))
 # Probability of obtaining outcome 1
 prob1=np.trace(np.dot(P1,np.dot(state2,np.conj(state2).T)))
+
 print(prob0,prob1)
