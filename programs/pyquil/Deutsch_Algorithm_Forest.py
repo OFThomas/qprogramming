c9c4b12e045ff8f94a3c4f9b781460956c339958
Oliver <oliver.thomas404@gmail.com>
2018-07-24 20:38:48 +0100

    formatting and reemoved long term

--- programs/pyquil/Deutsch_Algorithm_Forest.py
+++ programs/pyquil/Deutsch_Algorithm_Forest.py
@@ -1,24 +1,24 @@
 import numpy as np
 from pyquil.quil import Program
-from pyquil.api import QVMConnection 
-from pyquil.gates import X,Z,Y,H,I 
+from pyquil.api import QVMConnection
+from pyquil.gates import X, Z, Y, H, I
 from pyquilcompiler import compiletoquil
 # Invoking and renaming
-qvm=QVMConnection()
-p=Program() 
+qvm = QVMConnection()
+p = Program()
 # Gate implementation
-p.inst(H(0),H(1)) 
+p.inst(H(0), H(1))
 # Assuming the given function give the matrix
-Ufma=np.diag([-1,1,-1,1]) 
-p.defgate("Uf",Ufma)
-p.inst(("Uf",0,1)) 
-p.inst(H(0),H(1))
+Ufma = np.diag([-1, 1, -1, 1])
+p.defgate("Uf", Ufma)
+p.inst(("Uf", 0, 1))
+p.inst(H(0), H(1))
 # Measurements
-p.measure(0,0)
-p.measure(1,1) 
+p.measure(0, 0)
+p.measure(1, 1)
 
 compiletoquil(p)
 # Running the program
-cr=[] 
-results=qvm.run(p,cr,4) 
+cr = []
+results = qvm.run(p, cr, 4)
 print(results)
--- programs/pyquil/one_qubit_generator_Forest.py
+++ programs/pyquil/one_qubit_generator_Forest.py
@@ -1,25 +1,25 @@
-from pyquil.quil  import Program 
-from pyquil.api   import QVMConnection 
-from pyquil.gates import H,PHASE
+from pyquil.quil import Program
+from pyquil.api import QVMConnection
+from pyquil.gates import H, PHASE
 
 from pyquilcompiler import compiletoquil
 
 import numpy as np
 # Invoking and renaming
-qvm=QVMConnection()
-p=Program()
+qvm = QVMConnection()
+p = Program()
 # Gate implementation
 p.inst(H(0))
-theta=np.pi/2
-p.inst(PHASE(theta,0))
+theta = np.pi / 2
+p.inst(PHASE(theta, 0))
 
 # Measurement
-p.measure(0,0)
-p.measure(1,1)
+p.measure(0, 0)
+p.measure(1, 1)
 # Running the program
 
 compiletoquil(p)
 
-cr=[]
-results=qvm.run(p,cr,4)
+cr = []
+results = qvm.run(p, cr, 4)
 print(results)
