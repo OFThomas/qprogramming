87fe807d6c344f01c954369eeb68cf0e00b2ac35
johnrscott <johnrscott0@gmail.com>
2018-07-27 10:58:39 +0100

    // to # in .asm

--- GUIcode/print.asm
+++ GUIcode/print.asm
@@ -1,7 +1,7 @@
-	// -- Printing to the console --	
-	//
+	# -- Printing to the console --	
+	#
 
-	// This is a memory section
+	# This is a memory section
 	.section .data
 msg:	.ascii "\nHello World\n"
 	.set size, 13
@@ -9,14 +9,14 @@ msg:	.ascii "\nHello World\n"
 	.section .text
 	.global _start
 _start:
-	// print msg
+	# print msg
 	movl $msg, %ecx
 	movl $size, %edx
 	movl $4, %eax
 	movl $0, %ebx
 	int $0x80
 	
-	// quit with exit code 0
+	# quit with exit code 0
 	movl $1, %eax
 	movl $0, %ebx
 	int $0x80
