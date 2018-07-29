16b2a003608452a11b7923ecaae0857f3b24b474
Oliver <oliver.thomas404@gmail.com>
2018-07-29 19:28:25 +0100

    broke the test for qiskit. Soz

--- programs/qiskit/test.py
+++ programs/qiskit/test.py
@@ -4,15 +4,18 @@ from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister, QuantumPr
 from qiskit import available_backends, execute
 from qiskit.tools.visualization import circuit_drawer
 
+# initialise quantum program
 qp = QuantumProgram()
 
-# Create a Quantum Register with 2 qubits.
-q = :q
-QuantumRegister(8)
-# Create a Classical Register with 2 bits.
-c = ClassicalRegister(8)
+# Create a qubyte, Quantum Register with 8 qubits.
+qbyte = qp.create_quantum_register('q1', 8)
+cbyte = qp.create_classical_register('c1', 8)
+
+:q
+:q
+:wq
 # Create a Quantum Circuit
-qc = QuantumCircuit(q, c)
+qc = qp.create_circuit('helloworld', [qbyte], [cbyte])
 
 # Add a H gate on qubit 0, putting this qubit in superposition.
 #qc.h(q[1])
@@ -20,10 +23,13 @@ qc = QuantumCircuit(q, c)
 # Add a CX (CNOT) gate on control qubit 0 and target qubit 1, putting
 # the qubits in a Bell state.
 #qc.cx(q[0], q[1])
-# Add a Measure gate to see the state.
-for i in range(3):
-    qc.measure(q[1], c[i])
 
+# Measure
+for i in range(0, 8):
+    qc.measure(qbyte[i], cbyte[i])
+
+print(qp.get_qasm('helloworld'))
+"""
 # See a list of available local simulators
 print("Local backends: ", available_backends({'local': True}))
 
@@ -43,3 +49,4 @@ print(sim_result.get_counts(qc))
 # print QASM
 
 print(qp.get_asm())
+"""
