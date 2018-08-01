1e75f22ece6c769aad064f7bf95bc6741ab269c2
Oliver <ot17409@bristol.ac.uk>
2018-08-01 14:53:44 +0100

    added qiskit and project q minimal examples

--- programs/asm/projectq_helloworld.py
+++ programs/asm/projectq_helloworld.py
@@ -2,27 +2,29 @@
 
 from projectq.backends import CommandPrinter
 from projectq.backends import Simulator
-from projectq.setups.default import get_engine_list
-from projectq.setups import restrictedgateset
+#from projectq.setups.default import get_engine_list
+#from projectq.setups import restrictedgateset
 from projectq import MainEngine
 from projectq.ops import X, Y, Z, Rz, Ry, CNOT, All, Measure
 
-#eng = MainEngine(backend=CommandPrinter(accept_input=False))
+# Q in binary
+bitstring = '01010001'
+
+eng = MainEngine(backend=CommandPrinter(accept_input=False))
 
 #engines = get_engine_list() + [CommandPrinter()]
 #eng = MainEngine(backend=Simulator(),engine_list=engines)
 
-engines = restrictedgateset.get_engine_list(one_qubit_gates=(Rz,Ry),
-                                            two_qubit_gates=(CNOT,))
-eng = MainEngine(backend=CommandPrinter(accept_input=False),
-                 engine_list=engines)
+#engines = restrictedgateset.get_engine_list(one_qubit_gates=(Rz,Ry),                                            two_qubit_gates=(CNOT,))
+#eng = MainEngine(backend=CommandPrinter(accept_input=False),engine_list=engines)
 
 
-q = eng.allocate_qureg(4)
-X | q[0]
-Y | q[1]
-Z | q[2]
-CNOT | (q[2], q[3])
-Measure | q[2]
+q = eng.allocate_qureg(8)
+X | q[1]
+X | q[3]
+X | q[7]
+
+for i in range(0,8):
+    Measure | q[i]
 
 eng.flush()
--- programs/asm/pyquil_helloworld.py
+++ programs/asm/pyquil_helloworld.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from pyquil.quil import Program
 from pyquil.api import QVMConnection
-from pyquil.gates import X, I
+from pyquil.gates import X
 from pyquilcompiler import compiletoquil
 
 # Q in binary
@@ -9,19 +9,19 @@ bitstring = '01010001'
 
 # Do quantum stuff now we have our bit string
 qvm = QVMConnection()
-prog = Program()
+qprog = Program()
 # make 8 individual bits for the 8 qubits
 
-prog.inst(X(1), X(3), X(7))
+qprog.inst(X(1), X(3), X(7))
 
 for i in range(0, 8):
-    prog.measure(i, i)
+    qprog.measure(i, i)
 
 # store measurement outcomes, can change number of shots
-results = qvm.run(prog)
+results = qvm.run(qprog)
 
 #if explicitprinting == 1:
-compiletoquil(prog)
+compiletoquil(qprog)
 
 print('\n#--------------------------------------------------------#')
 print('# Result =', results[0])
