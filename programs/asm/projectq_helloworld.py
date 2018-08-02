70412e63205f93994275e28f9fab17e9effb3de9
Oliver <oliver.thomas404@gmail.com>
2018-08-02 20:59:03 +0100

    added project q code for helloworld

--- programs/asm/projectq_helloworld.py
+++ programs/asm/projectq_helloworld.py
@@ -1,35 +1,47 @@
 #!/usr/bin/env python3
 
 from projectq import MainEngine
-from projectq.ops import X, Y, Z, Rz, Ry, All, Measure
+from projectq.ops import X, Z, H, All, Measure
 from projectq.backends import CommandPrinter
 from projectq.backends import Simulator
 
-# Q in binary
-bitstring = '01010001'
-
-eng = MainEngine(backend=CommandPrinter(accept_input=False))
-
-#eng = MainEngine()
-#engines = get_engine_list() + [CommandPrinter()]
-#eng = MainEngine(backend=Simulator(),engine_list=engines)
-
-#engines = restrictedgateset.get_engine_list(one_qubit_gates=(Rz,Ry),                                            two_qubit_gates=(CNOT,))
-#eng = MainEngine(backend=CommandPrinter(accept_input=False),engine_list=engines)
-
-q = eng.allocate_qureg(8)
-
-X | q[1]
-X | q[3]
-X | q[7]
-
-All(Measure) | q
-
-eng.flush()
+# specify compiler
+compiler = MainEngine(backend=CommandPrinter(accept_input=False))
+# specify simulate locally
+simulate = MainEngine(backend=Simulator())
 
 
-results=[]
-for element in q:
-    results.append(int(q[int(element)]))
-print(results)
+def qprogram(eng):
+    q = eng.allocate_qureg(8)
 
+    # Q in binary
+    # bitstring = '01010001'
+
+    # recall H Z H is X
+    H | q[1]
+    Z | q[1]
+    H | q[1]
+
+    X | q[3]
+    X | q[7]
+
+    # measure all the qubits
+    All(Measure) | q
+
+    # execute the quantum program
+    eng.flush()
+
+    # only print the results if it has been simulated
+    if eng == simulate:
+        results = []
+        # the values of qubits is stored as the
+        # integer value of the object
+        for element in q:
+            results.append(int(q[int(element)]))
+        print(results)
+
+
+# run for the compiler to see what the program does
+qprogram(compiler)
+# then simulate
+qprogram(simulate)
