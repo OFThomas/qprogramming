42f1864e50985181829dea2e373f3fc1cc3e9d2f
Oliver <oliver.thomas404@gmail.com>
2018-08-02 20:25:42 +0100

    pyquil hello world formatting

--- programs/asm/pyquil_helloworld.py
+++ programs/asm/pyquil_helloworld.py
@@ -2,7 +2,7 @@
 from pyquil.quil import Program
 from pyquil.api import QVMConnection
 from pyquil.gates import X, Z, H
-from pyquilcompiler import compiletoquil
+from compile_for_pyquil import compiletoquil
 
 # Q in binary
 bitstring = '01010001'
@@ -10,22 +10,21 @@ bitstring = '01010001'
 # Do quantum stuff now we have our bit string
 qvm = QVMConnection()
 qprog = Program()
-# make 8 individual bits for the 8 qubits
 
+# do X on q1, q3, q7
+# remember H Z H is X
 qprog.inst(H(1), Z(1), H(1))
 qprog.inst(X(3))
 qprog.inst(X(7))
 
-
+# do measurement over all 8 qubits
 for i in range(0, 8):
-   qprog.measure(i, i)
+    qprog.measure(i, i)
 
-# store measurement outcomes, can change number of shots
+# store measurement outcomes
 results = qvm.run(qprog)
 
-#if explicitprinting == 1:
+# show info when compiled to 8 qubit AGAVE
 compiletoquil(qprog)
 
-print('\n#--------------------------------------------------------#')
 print('# Result =', results[0])
-print('#--------------------------------------------------------#')
